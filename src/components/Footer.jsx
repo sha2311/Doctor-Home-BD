a8be24179b7bb0a700cc4c78c7a1243e8b784b4b
sha2311 <sha31417@gmail.com>
2024-12-07 12:49:27 +0600

    name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024 @ Doctor Home BD - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2024 @ Sheikh Hashibub Alam - All Right Reserved.</p>
         </div>
     </div>
   )
